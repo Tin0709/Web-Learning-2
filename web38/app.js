451e110d2598cd9b0c1b576cc9d347988b917686
Tin0709 <2002trungtin@gmail.com>
2025-09-17 23:00:51 +0700

    web38 git setup DOM

---
+++ web38/app.js
@@ -0,0 +1,9 @@
+const DOM = {
+  jokeArea: document.getElementById("jokeArea"),
+  newBtn: document.getElementById("newJokeBtn"),
+  copyBtn: document.getElementById("copyBtn"),
+  speakBtn: document.getElementById("speakBtn"),
+  toast: document.getElementById("toast"),
+  themeToggle: document.getElementById("themeToggle"),
+  card: document.querySelector(".card"),
+};
