c8a17b53da5d82d9dcc8f3cc3d8293d897e1d3bb
unknown <2002trungtin@gmail.com>
2025-09-21 15:35:00 +0700

    web40 adding elements datas

---
+++ web40/script.js
@@ -0,0 +1,9 @@
+// Select elements
+const bookForm = document.getElementById("bookForm");
+const booksList = document.getElementById("books");
+
+// Book data (temporary, stored in memory)
+let books = [
+  { title: "1984", author: "George Orwell", votes: 5 },
+  { title: "To Kill a Mockingbird", author: "Harper Lee", votes: 3 },
+];
