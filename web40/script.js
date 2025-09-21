929b6e2daeb3c6bd12f332dfe8fd5b5f7efad34e
unknown <2002trungtin@gmail.com>
2025-09-21 15:35:18 +0700

    web40 adding Render books logics

--- web40/script.js
+++ web40/script.js
@@ -7,3 +7,26 @@ let books = [
   { title: "1984", author: "George Orwell", votes: 5 },
   { title: "To Kill a Mockingbird", author: "Harper Lee", votes: 3 },
 ];
+// Render books
+function renderBooks() {
+  booksList.innerHTML = "";
+  books
+    .sort((a, b) => b.votes - a.votes) // sort by votes
+    .forEach((book, index) => {
+      const li = document.createElement("li");
+      li.className = "book-item";
+
+      li.innerHTML = `
+        <div class="book-info">
+          <h3>${book.title}</h3>
+          <p>by ${book.author}</p>
+          <p>Votes: <strong>${book.votes}</strong></p>
+        </div>
+        <div class="vote-buttons">
+          <button onclick="vote(${index}, 1)">👍</button>
+          <button onclick="vote(${index}, -1)">👎</button>
+        </div>
+      `;
+      booksList.appendChild(li);
+    });
+}
