77cd975ae8e619abf332eed93f6b43388514151f
Tin0709 <2002trungtin@gmail.com>
2025-09-22 08:35:26 +0700

    web41 basic func

--- web41/app.js
+++ web41/app.js
@@ -6,3 +6,10 @@ const $$ = (sel, ctx = document) => Array.from(ctx.querySelectorAll(sel));
 const state = {
   columns: [],
 };
+function uid(prefix = "id") {
+  return `${prefix}_${Math.random().toString(36).slice(2, 9)}`;
+}
+
+function save() {
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(state));
+}
