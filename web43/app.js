178a7baee947711551eeed285ec884cccdbe9086
Tin0709 <2002trungtin@gmail.com>
2025-09-24 09:33:05 +0700

    web43 adding more Utilities

--- web43/app.js
+++ web43/app.js
@@ -41,3 +41,36 @@ const weatherIcon = (code, isNight = false) => {
   }
   return map[code] || "🌡️";
 };
+const codeToText = (code) => {
+  const dict = {
+    0: "Clear sky",
+    1: "Mainly clear",
+    2: "Partly cloudy",
+    3: "Overcast",
+    45: "Fog",
+    48: "Depositing rime fog",
+    51: "Light drizzle",
+    53: "Moderate drizzle",
+    55: "Dense drizzle",
+    56: "Light freezing drizzle",
+    57: "Dense freezing drizzle",
+    61: "Slight rain",
+    63: "Moderate rain",
+    65: "Heavy rain",
+    66: "Light freezing rain",
+    67: "Heavy freezing rain",
+    71: "Slight snow",
+    73: "Moderate snow",
+    75: "Heavy snow",
+    77: "Snow grains",
+    80: "Rain showers",
+    81: "Heavy rain showers",
+    82: "Violent rain showers",
+    85: "Snow showers",
+    86: "Heavy snow showers",
+    95: "Thunderstorm",
+    96: "Thunderstorm w/ hail",
+    99: "Thunderstorm w/ hail",
+  };
+  return dict[code] || "—";
+};
