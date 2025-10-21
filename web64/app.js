0c33df3d9891f1ffe6da0daef09abe3cd0ee9530
Tin0709 <2002trungtin@gmail.com>
2025-10-21 17:42:21 +0700

    web64 adding logics

--- web64/app.js
+++ web64/app.js
@@ -1,5 +1,5 @@
 /* ========= CONFIG ========= */
-const API_KEY = "YOUR_TMDB_API_KEY"; // <-- replace with your TMDB key
+const API_KEY = "Real_TMDB_API_KEY"; // <-- replace with your TMDB key
 const API_ROOT = "https://api.themoviedb.org/3";
 const IMG_BASE = "https://image.tmdb.org/t/p/";
 const POSTER_SIZE = "w342"; // good balance for grids
